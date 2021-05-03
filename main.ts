df867f4646fce8014054f47b447489ca3d2a6582
simoneeffe <72350400+simoneeffe@users.noreply.github.com>
2021-05-03 10:25:08 +0200

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
-control.onEvent(EventBusSource.MICROBIT_ID_GESTURE, EventBusValue.MES_DEVICE_GESTURE_DEVICE_SHAKEN, function () {
+input.onGesture(Gesture.Shake, function () {
     basic.showLeds(`
-        . # # . .
-        . . . . #
-        . # . . .
-        . . . # .
         # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # . . .
         `)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_GESTURE, EventBusValue.MES_DEVICE_GESTURE_DEVICE_SHAKEN, function () {
+	
+})
+basic.forever(function () {
+	
 })
